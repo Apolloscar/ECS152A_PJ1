54ca5c9a2a6e210daf5b88669f900c71cd8c74ca
Apolloscar <ohernandez4869@gmail.com>
2022-10-16 19:24:31 -0700

    q1 is better

--- PJ_P1.py
+++ PJ_P1.py
@@ -4,24 +4,70 @@ fy = open('youtube.pcap', 'rb')
 pcapy = dpkt.pcap.Reader(fy)
 you_q1_TCP = 0
 you_q1_UDP = 0
-
+you = 0
     
-for ts,buf in pcapy:
+for ts, buf in pcapy:
+
     eth = dpkt.ethernet.Ethernet(buf)
+    you += 1
+    if not isinstance(eth.data, dpkt.ip.IP) :
+        print('Non IP Packet type not supported %s' % eth.data.__class__.__name__)
+        continue
+
     ip = eth.data
-    ans = str(repr(ip.data))
-    i = 0
-    got = ""
-    while i < len(ans) and ans[i] != "(":
-        got = got + ans[i]
-        i = i +1
+    tcp = ip.data
+    if isinstance(tcp, dpkt.tcp.TCP):
+        you_q1_TCP += 1
+    if isinstance(tcp, dpkt.udp.UDP):
+        you_q1_UDP += 1
 
-    if got == "TCP":
-        you_q1_TCP = you_q1_TCP + 1
-    if got == "UDP":
-        you_q1_UDP = you_q1_UDP + 1
-        
-print(you_q1_TCP, you_q1_UDP)
+print("Youtube TCP packets:", you_q1_TCP)
+print("Youtube UDP packets:", you_q1_UDP)
+
 ################################################################################
+fy.close()
+fy = open('youtube.pcap', 'rb')
+pcapy = dpkt.pcap.Reader(fy)
+you = 0
+for ts, buf in pcapy:
+    
+    eth = dpkt.ethernet.Ethernet(buf)
+  
+    if not isinstance(eth.data, dpkt.ip.IP):
+        continue
+
+    ip = eth.data
+    tcp = ip.data
+    
+
+    """
+    else:
+              
 
 
+        if tcp.dport == 80 and len(tcp.data) > 0:
+            print('HTTP Request')
+
+            print('Source Port: %s' % tcp.sport)
+            print('Destination Port: %s' % tcp.dport)
+            if len(tcp.data) > 0:
+                try:
+                    http = dpkt.http.Request(tcp.data)
+                    print(http)
+                except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
+                    continue
+        elif tcp.sport == 80 and len(tcp.data) >= 0:
+            print('HTTP Response')
+
+            print('Source Port: %s' % tcp.sport)
+            print('Destination Port: %s' % tcp.dport)
+            if len(tcp.data) > 0:
+                try:
+                    http = dpkt.http.Response(tcp.data)
+                    print(http.body.decode())
+                except (dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
+                    continue
+        else:
+
+            continue
+        """
