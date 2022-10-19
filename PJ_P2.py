229204d77a2aac10925915a1c7f77498a2543350
Apolloscar <86376444+Apolloscar@users.noreply.github.com>
2022-10-18 21:00:28 -0700

    Update PJ_P2.py

--- PJ_P2.py
+++ PJ_P2.py
@@ -22,6 +22,11 @@ device_sent = [0]*256
 device_recieved = [0]*256
 
 
+endpoint_printed = []
+
+device_dest = [[] for Null in range(256)]
+
+
 for ts, buf in pcap:
     
     eth = dpkt.ethernet.Ethernet(buf)
@@ -32,42 +37,84 @@ for ts, buf in pcap:
         continue
     ip = eth.data
     ipa_src = str(inet_to_str(ip.src))
-    ipa_Sections = [""] *4
-    ipa_Sections_integers = [0]*4
+    ipa_Sections_src = [""] *4
+    ipa_Sections_integers_src = [0]*4
     i =0 
     j = 0
     while i < len(ipa_src):
         if ipa_src[i] == '.':
             j += 1
             i = i+1
-        ipa_Sections[j] += ipa_src[i]
+        ipa_Sections_src[j] += ipa_src[i]
         i += 1
 
-    ipa_Sections_integers[0] = int(ipa_Sections[0])
-    ipa_Sections_integers[1] = int(ipa_Sections[1])
-    ipa_Sections_integers[2] = int(ipa_Sections[2])
-    ipa_Sections_integers[3] = int(ipa_Sections[3])
-    if ipa_Sections_integers[0] == 10 and ipa_Sections_integers[1] == 42 and ipa_Sections_integers[2] == 0 and ipa_Sections_integers[3] > 1 and ipa_Sections_integers[3] < 256:
-        device_sent[ipa_Sections_integers[3]] += 1
+    ipa_Sections_integers_src[0] = int(ipa_Sections_src[0])
+    ipa_Sections_integers_src[1] = int(ipa_Sections_src[1])
+    ipa_Sections_integers_src[2] = int(ipa_Sections_src[2])
+    ipa_Sections_integers_src[3] = int(ipa_Sections_src[3])
+
+    
+    
 
     ipa_dst = str(inet_to_str(ip.dst))
-    ipa_Sections= [""] *4
-    ipa_Sections_integers = [0]*4
+    ipa_Sections_dst= [""] *4
+    ipa_Sections_integers_dst = [0]*4
     i =0 
     j = 0
     while i < len(ipa_dst):
         if ipa_dst[i] == '.':
             j += 1
             i = i+1
-        ipa_Sections[j] += ipa_dst[i]
+        ipa_Sections_dst[j] += ipa_dst[i]
         i += 1
 
-    ipa_Sections_integers[0] = int(ipa_Sections[0])
-    ipa_Sections_integers[1] = int(ipa_Sections[1])
-    ipa_Sections_integers[2] = int(ipa_Sections[2])
-    ipa_Sections_integers[3] = int(ipa_Sections[3])
-    if ipa_Sections_integers[0] == 10 and ipa_Sections_integers[1] == 42 and ipa_Sections_integers[2] == 0 and ipa_Sections_integers[3] > 1 and ipa_Sections_integers[3] < 256:
-        device_recieved[ipa_Sections_integers[3]] += 1
+    ipa_Sections_integers_dst[0] = int(ipa_Sections_dst[0])
+    ipa_Sections_integers_dst[1] = int(ipa_Sections_dst[1])
+    ipa_Sections_integers_dst[2] = int(ipa_Sections_dst[2])
+    ipa_Sections_integers_dst[3] = int(ipa_Sections_dst[3])
+
+
+    if ipa_Sections_integers_src[0] == 10 and ipa_Sections_integers_src[1] == 42 and ipa_Sections_integers_src[2] == 0 and ipa_Sections_integers_src[3] > 1 and ipa_Sections_integers_src[3] < 256:
+        device_sent[ipa_Sections_integers_src[3]] += 1
+
+        printer = 1
+        for k in endpoint_printed:
+            if k == ipa_dst:
+                printer = 0
+        
+        if printer == 1:
+
+            found = 0
+            n = 2
+
+    
+            while n < len(device_dest) and found == 0:
+                
+                if n == ipa_Sections_integers_src[3]:
+                    n += 1
+
+                if n >= len(device_dest):
+                    break
+
+                for m in device_dest[n]:
+                    if m == ipa_dst:
+                        found = 1
+                        break
+                n += 1
+
+            if found == 1:
+                endpoint_printed.append(ipa_dst)
+                
+            else:
+                device_dest[ipa_Sections_integers_src[3]].append(ipa_dst)
+                
+
+          
+
+            
+
+    if ipa_Sections_integers_dst[0] == 10 and ipa_Sections_integers_dst[1] == 42 and ipa_Sections_integers_dst[2] == 0 and ipa_Sections_integers_dst[3] > 1 and ipa_Sections_integers_dst[3] < 256:
+        device_recieved[ipa_Sections_integers_dst[3]] += 1
 
 devices = 0
 
@@ -86,7 +133,14 @@ most_packets_recieved = max(device_recieved)
 index_of_most_packets_recieved = device_recieved.index(most_packets_recieved)
 
 print("IP adress of device with most recieved packets is: 10.42.0." + str(index_of_most_packets_recieved))
+
+print("IP adress that at least two devices have sent to:")
+
+i = 1
+for j in endpoint_printed:
+    print(str(i) + ".\t" + j)
+    i += 1
+
+
     
 f.close()
-    
-    
